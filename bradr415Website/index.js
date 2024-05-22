15c9b7ca9bced13634a402d3a7ad940557a6a94c
Bradley Royal <royalsbr@ad.unc.edu>
2024-05-22 17:20:45 +0200

    forwarding my initial website to my GitHub repo

---
+++ bradr415Website/index.js
@@ -0,0 +1,12 @@
+function fibonacciSequence(){
+    let list = [0, 1];
+    
+    for (let i = 2; i < 10; i++) {
+      list[i] = list[i - 1] + list[i - 2];
+    }
+    
+    return list;
+  }
+  
+  
+  console.log(fibonacciSequence);
