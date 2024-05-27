22668b317a22e55e8f94dfda89fceb5f2f6034f9
Bradley Royal <royalsbr@ad.unc.edu>
2024-05-27 10:28:58 +0200

    Uploading the blog website I have currently

--- ix-blog/src/compnents/Heading/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Heading() {
-  return <p className="page-title">THE BLOG</p>;
-}
--- ix-blog/src/compnents/subheading/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function SubHeading({ subHeading }) {
-  return <p className="page-subtitle">{subHeading}</p>;
-}
