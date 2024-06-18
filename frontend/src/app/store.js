f5d8d1f1dce58d48443f1afbfe0f2c736d19b2a3
Bradley Royal <royalsbr@ad.unc.edu>
2024-06-18 17:50:32 +0200

    Updating the blog app

---
+++ frontend/src/app/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/authSlice";
+import blogReducer from "../features/blogsSlice";
+import categoryReducer from "../features/categoriesSlice";
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    blogs: blogReducer,
+    categories: categoryReducer,
+  },
+});
+
+export default store;
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import Navbar from "../../components/Navbar";
 import Heading from "../../components/Heading";
@@ -6,12 +7,56 @@ import SubHeading from "../../components/Subheading";
 import BlogGrid from "../../components/BlogGrid";
 import CategoriesList from "../../components/CategoriesList";
 import Footer from "../../components/Footer";
+import Loading from "../../components/Loading";
+import SuccessToast from "../../components/SuccessToast";
+import ErrorToast from "../../components/ErrorToast";
 
-const data = require("../../dummy-data.json");
-const blogs = data.blogPosts;
-const categories = data.categories;
+import {
+  fetchBlogs,
+  resetSuccessAndError as resetBlogSuccessAndError,
+} from "../../features/blogsSlice";
+import {
+  fetchCategories,
+  resetSuccessAndError as resetCategoriesSuccessAndError,
+} from "../../features/categoriesSlice";
 
-export default function index() {
+export default function Home() {
+  const dispatch = useDispatch();
+
+  const {
+    blogs,
+    isError: isBlogsError,
+    isSuccess: isBlogsSuccess,
+    isLoading: isLoadingBlogs,
+    message: blogsMessage,
+  } = useSelector((state) => state.blogs);
+
+  const {
+    categories,
+    isError: isCategoriesError,
+    isSuccess: isCategoriesSuccess,
+    isLoading: isLoadingCategories,
+    message: categoriesMessage,
+  } = useSelector((state) => {
+    return state.categories;
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch(fetchBlogs());
+        dispatch(fetchCategories());
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (isLoadingBlogs || isLoadingCategories) {
+    return <Loading />;
+  }
+
   return (
     <>
       <Navbar />
@@ -22,6 +67,22 @@ export default function index() {
         <CategoriesList categories={categories} />
         <Footer />
       </div>
+      <SuccessToast
+        show={isBlogsSuccess || isCategoriesSuccess}
+        message={blogsMessage || categoriesMessage}
+        onClose={() => {
+          dispatch(resetBlogSuccessAndError());
+          dispatch(resetCategoriesSuccessAndError());
+        }}
+      />
+      <ErrorToast
+        show={isBlogsError || isCategoriesError}
+        message={blogsMessage || categoriesMessage}
+        onClose={() => {
+          dispatch(resetBlogSuccessAndError());
+          dispatch(resetCategoriesSuccessAndError());
+        }}
+      />
     </>
   );
 }
